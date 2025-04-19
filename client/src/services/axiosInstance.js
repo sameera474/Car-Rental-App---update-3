c7b5a0003445ccc857d3d56c722606064db7bead
Sameera Chaturanga Wagaarachchige <ag1912@student.jamk.fi>
2025-04-20 01:41:46 +0300

    Changed front end .evn

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -2,37 +2,32 @@
 import axios from "axios";
 import { getToken, removeToken } from "../utils/token";
 
-// Define local and production API URLs.
+// Local dev URL
 const localApiUrl = "http://localhost:5000/api";
-const prodApiUrl = import.meta.env.VITE_API_URL; // Should be set to your production API (e.g., "https://sam-car-rent-service-backend.vercel.app/api")
 
-// Determine which API URL to use based on the hostname.
-const API_URL =
-  window.location.hostname === "localhost" ? localApiUrl : prodApiUrl;
+// Vite only exposes vars prefixed with VITE_
+const prodApiUrl = import.meta.env.VITE_API_URL;
+
+// If you're running `vite dev` then import.meta.env.DEV is true
+const API_URL = import.meta.env.DEV ? localApiUrl : prodApiUrl;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
-    Authorization: localStorage.getItem("token")
-      ? `Bearer ${localStorage.getItem("token")}`
-      : "",
+    Authorization: getToken() ? `Bearer ${getToken()}` : "",
   },
 });
 
-// Add request interceptor
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = getToken();
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+    if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
   },
   (error) => Promise.reject(error)
 );
 
-// Add response interceptor to handle 401 errors
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
