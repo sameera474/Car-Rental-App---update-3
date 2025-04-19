ca877b5e8f3c7485d31236cf6a9c7c7ad197927f
Sameera Chaturanga Wagaarachchige <ag1912@student.jamk.fi>
2025-04-20 01:13:37 +0300

    Again changed the routes inside the index.js file

--- server/api/index.js
+++ server/api/index.js
@@ -2,21 +2,21 @@
 import serverless from "serverless-http";
 import express from "express";
 import cors from "cors";
-import connectDB from "../server/config/db.js";
-import authRoutes from "../server/routes/authRoutes.js";
-import userRoutes from "../server/routes/userRoutes.js";
-import carRoutes from "../server/routes/carRoutes.js";
-import rentalRoutes from "../server/routes/rentalRoutes.js";
-import reviewRoutes from "../server/routes/reviewRoutes.js";
-import bossRoutes from "../server/routes/bossRoutes.js";
-import adminRoutes from "../server/routes/adminRoutes.js";
+import connectDB from "../config/db.js";
+import authRoutes from "../routes/authRoutes.js";
+import userRoutes from "../routes/userRoutes.js";
+import carRoutes from "../routes/carRoutes.js";
+import rentalRoutes from "../routes/rentalRoutes.js";
+import reviewRoutes from "../routes/reviewRoutes.js";
+import bossRoutes from "../routes/bossRoutes.js";
+import adminRoutes from "../routes/adminRoutes.js";
 
 const app = express();
 connectDB();
 app.use(express.json());
 app.use(cors());
 // serve uploads
-app.use("/uploads", express.static("./server/uploads"));
+app.use("/uploads", express.static("./uploads"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
